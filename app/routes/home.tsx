9688b3bf02f243c85c0853a08ba39de0a0aa5c17
Asterixter <114302678+Harishvjk@users.noreply.github.com>
2025-08-22 13:51:11 +0530

    Home page and Nav

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    {
+      name: "description",
+      content: "Get personalized feedback on your resume!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar></Navbar>
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Boost Your Career By Making Your Resume Perfect</h1>
+          <h2>Get AI-powered feedback on your applications</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
